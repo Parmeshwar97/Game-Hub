e57c2401d8c6726db54028b6578286fa2795499d
Parmeshwar <parmeshwarpatil9763@gmail.com>
2024-12-22 22:51:22 +0530

    Building search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import logo from '../assets/logo.webp'
+import logo from "../assets/logo.webp";
+import SearchInput from "./SearchInput";
 const NavBar = () => {
-    return (
-        <nav className='flex items-center'>
-            <img src={logo} alt="website-logo" className='w-20' />
-            <p>Navbar</p>
-        </nav>
-    )
-}
+  return (
+    <nav className="flex items-center">
+      <img src={logo} alt="website-logo" className="w-20" />
+      <SearchInput />
+    </nav>
+  );
+};
 
 export default NavBar;
---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,12 @@
+
+
+const SearchInput  = () => {
+  return (
+    <input
+      placeholder="Search games..."
+      className="px-5 py-2.5 text-lg w-10/12 mx-3 rounded-3xl bg-neutral-800 placeholder-zinc-600"
+    />
+  );
+}
+
+export default SearchInput;
