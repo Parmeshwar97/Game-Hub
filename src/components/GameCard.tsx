534dbd03e793a63a8ab1ded4298ca82125d8fa65
Parmeshwar <parmeshwarpatil9763@gmail.com>
2024-12-26 06:44:20 +0530

    Refactor Colors

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -9,13 +9,20 @@ export interface Props {
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <div className="bg-neutral-900 rounded-lg border-2 border-neutral-900 max-h-fit overflow-hidden">
+    <div
+      className="
+    bg-[#202020] rounded-lg border-2
+
+    border-neutral-900 max-h-fit overflow-hidden"
+    >
       <img src={getCroppedImage(game.background_image)} className="w-full" />
+
       <div className="p-5 flex flex-col gap-y-2">
         <div className="flex items-center mb-2 justify-between">
           <IconsList game={game} />
           <CriticScore score={game.metacritic} />
         </div>
+
         <p className="font-extrabold text-xl">
           {game.name}
           <Emoji rating={game.rating_top} />
