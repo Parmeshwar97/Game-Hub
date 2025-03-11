494446c38751fe33ad373218401b348847e96daf
Parmeshwar Patil <parmeshwarpatil9763@gmail.com>
2025-03-11 20:46:04 +0530

    made logo dynamic

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.webp";
 import DarkMode from "./DarkMode";
 import SearchInput from "./SearchInput";
@@ -5,7 +6,7 @@ import SearchInput from "./SearchInput";
 const NavBar = () => {
   return (
     <nav className="flex items-center py-2 justify-between">
-      <img src={logo} alt="website-logo" className="size-16" />
+      <Link to='/'><img src={logo} alt="website-logo" className="size-16" /></Link>
       <SearchInput  />
       <DarkMode  />
     </nav>
